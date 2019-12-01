433220d71a243ccfad92d5d8529d50938c7cbd31
Bronson Quick <bronson@bronsonquick.com.au>
2019-12-01 10:36:31 +1000

    Initial commit

---
+++ lib/chassis/updater.rb
@@ -0,0 +1,8 @@
+require "chassis/updater/version"
+
+module Chassis
+  module Updater
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/chassis/updater/version.rb
@@ -0,0 +1,5 @@
+module Chassis
+  module Updater
+    VERSION = "0.1.0"
+  end
+end
